07df7209181421fae992f19ff98275f2883c7d3f
Edward Diehl <EdwDiehl@gmail.com>
2017-04-09 17:04:44 +0300

    Add gulp-sass package

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 
-gulp.task('lint-css', () => {
+gulp.task('lint-scss', () => {
   const stylelint = require('gulp-stylelint');
 
-  return gulp.src('src/**/*.*css')
+  return gulp.src('./src/scss/**/*.*css')
     .pipe(stylelint({
       failAfterError: false, // disable fail after error
       reporters: [{
@@ -12,3 +13,13 @@ gulp.task('lint-css', () => {
       }]
     }));
 });
+
+gulp.task('scss', function () {
+  return gulp.src('./src/scss/**/*.*css')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('./src/scss/**/*.*css', ['lint-scss', 'scss']);
+})
